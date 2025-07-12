fe46a3e120a6c8462b76a5a01c62c09ba2b78373
Usama Imdad <usamaimdadsian@gmail.com>
2025-07-12 08:24:33 +0500

    debuggin added code to create launch json file

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,7 +7,7 @@ local map = LazyVim.safe_keymap_set
 map("i", "kj", "<Esc>", { desc = "Go to normal mode" })
 map("v", "kj", "<Esc>", { desc = "Go to normal mode" })
 map("n", "<leader>bn", ":enew<CR>", { desc = "New buffer" })
-map("n", "<leader>bz", ":ZenMode<CR>", { desc = "toggle zen mode (zoom in current window)" })
+map("n", "<leader>bz", ":ZenMode<CR>", { desc = "toggle zen mode (zoom in current window/buffer)" })
 map("n", "<leader>dU", function()
   require("dapui").toggle({ reset = true })
 end, { desc = "DAP UI reset" })
--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -1,3 +1,44 @@
+local function handle_continue()
+  local cwd = vim.fn.getcwd()
+  local vscode_dir = cwd .. "/.vscode"
+  local launch_path = vscode_dir .. "/launch.json"
+
+  -- Create .vscode directory if it doesn't exist
+  if vim.fn.isdirectory(vscode_dir) == 0 then
+    vim.fn.mkdir(vscode_dir, "p")
+  end
+
+  -- Create launch.json file if it doesn't exist
+  if vim.fn.filereadable(launch_path) == 0 then
+    local file = io.open(launch_path, "w")
+    if file then
+      file:write([[
+{
+  "version": "0.2.0",
+  "configurations": [
+    {
+      "name": "Run Project",
+      "type": "pwa-node/debugpy",
+      "request": "launch",
+      "program": "${workspaceFolder}/",
+      "cwd": "${workspaceFolder}",
+      "console": "integratedTerminal"
+    }
+  ]
+}
+      ]])
+      file:close()
+    else
+      vim.notify("Failed to create launch.json", vim.log.levels.ERROR)
+      return
+    end
+    -- Open launch.json in a buffer
+    vim.cmd("edit " .. vim.fn.fnameescape(launch_path))
+  else
+    require("dap").continue()
+  end
+end
+
 return {
   {
     "Weissle/persistent-breakpoints.nvim",
@@ -10,9 +51,11 @@ return {
 
       local opts = { noremap = true, silent = true }
       local keymap = vim.api.nvim_set_keymap
+      _G.handle_continue = handle_continue
 
       keymap("n", "<leader>db", "<cmd>lua require('persistent-breakpoints.api').toggle_breakpoint()<CR>", opts)
       keymap("n", "<leader>dB", "<cmd>lua require('persistent-breakpoints.api').set_conditional_breakpoint()<CR>", opts)
+      keymap("n", "<leader>dc", "<cmd>lua handle_continue()<CR>", opts)
       -- keymap("n", "<leader>dl", "<cmd>lua require('persistent-breakpoints.api').set_log_point()<CR>", opts)
       -- keymap("n", "<leader>dC", "<cmd>lua require('persistent-breakpoints.api').clear_all_breakpoints()<CR>", opts)
     end,
