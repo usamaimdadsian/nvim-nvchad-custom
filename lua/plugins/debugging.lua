23ee320e858234e57dfab10d786c82ed2eaeb3b6
Usama Imdad <usamaimdadsian@gmail.com>
2025-07-12 05:41:15 +0500

    separated debugging, added nvim-surround

---
+++ lua/plugins/debugging.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    "Weissle/persistent-breakpoints.nvim",
+    event = "BufReadPost", -- load breakpoints when reading files
+    dependencies = { "mfussenegger/nvim-dap" }, -- make sure DAP is installed
+    config = function()
+      require("persistent-breakpoints").setup({
+        load_breakpoints_event = { "BufReadPost" },
+      })
+
+      local opts = { noremap = true, silent = true }
+      local keymap = vim.api.nvim_set_keymap
+
+      keymap("n", "<leader>db", "<cmd>lua require('persistent-breakpoints.api').toggle_breakpoint()<CR>", opts)
+      keymap("n", "<leader>dB", "<cmd>lua require('persistent-breakpoints.api').set_conditional_breakpoint()<CR>", opts)
+      -- keymap("n", "<leader>dl", "<cmd>lua require('persistent-breakpoints.api').set_log_point()<CR>", opts)
+      -- keymap("n", "<leader>dC", "<cmd>lua require('persistent-breakpoints.api').clear_all_breakpoints()<CR>", opts)
+    end,
+  },
+  {
+    "mfussenegger/nvim-dap",
+    keys = {
+      {
+        "<leader>do",
+        function()
+          require("dap").step_over()
+        end,
+        desc = "Step Over",
+      },
+      {
+        "<leader>dO",
+        function()
+          require("dap").step_out()
+        end,
+        desc = "Step Out",
+      },
+    },
+  },
+}
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,44 +4,22 @@ return {
     dependencies = { "nvim-neotest/nvim-nio" },
   },
   {
-    "Weissle/persistent-breakpoints.nvim",
-    event = "BufReadPost", -- load breakpoints when reading files
-    dependencies = { "mfussenegger/nvim-dap" }, -- make sure DAP is installed
-    config = function()
-      require("persistent-breakpoints").setup({
-        load_breakpoints_event = { "BufReadPost" },
-      })
-
-      local opts = { noremap = true, silent = true }
-      local keymap = vim.api.nvim_set_keymap
-
-      keymap("n", "<leader>db", "<cmd>lua require('persistent-breakpoints.api').toggle_breakpoint()<CR>", opts)
-      keymap("n", "<leader>dB", "<cmd>lua require('persistent-breakpoints.api').set_conditional_breakpoint()<CR>", opts)
-      -- keymap("n", "<leader>dl", "<cmd>lua require('persistent-breakpoints.api').set_log_point()<CR>", opts)
-      -- keymap("n", "<leader>dC", "<cmd>lua require('persistent-breakpoints.api').clear_all_breakpoints()<CR>", opts)
-    end,
-  },
-  {
-    "mfussenegger/nvim-dap",
-    keys = {
-      {
-        "<leader>do",
-        function()
-          require("dap").step_over()
-        end,
-        desc = "Step Over",
-      },
-      {
-        "<leader>dO",
-        function()
-          require("dap").step_out()
-        end,
-        desc = "Step Out",
-      },
-    },
-  },
-  {
     "folke/zen-mode.nvim",
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({})
+      -- surr*ound_words             ysiw)           (surround_words)
+      -- *make strings               ys$"            "make strings"
+      -- [delete ar*ound me!]        ds]             delete around me!
+      -- remove <b>HTML t*ags</b>    dst             remove HTML tags
+      -- 'change quot*es'            cs'"            "change quotes"
+      -- <b>or tag* types</b>        csth1<CR>       <h1>or tag types</h1>
+      -- delete(functi*on calls)     dsf             function calls
+    end,
   },
   -- {
   --   "m4xshen/hardtime.nvim",
