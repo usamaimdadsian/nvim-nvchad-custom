b59049476905574a71ecbcb7e11f37a4bd2411f0
Usama Imdad <usamaimdadsian@gmail.com>
2025-12-08 13:49:01 +0500

    lazy load by default

---
+++ lua/plugins/obsidian.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*", -- recommended, use latest release instead of latest commit
+    -- lazy = true,
+    ft = "markdown",
+    -- event = {
+    --   "BufReadPre " .. vim.fn.expand("~") .. "/linux/obsidian/obsidian-docs/*.md",
+    --   "BufNewFile " .. vim.fn.expand("~") .. "/linux/obsidian/obsidian-docs/*.md",
+    -- },
+
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      picker = { name = "snacks.picker" },
+      workspaces = {
+        {
+          name = "personal",
+          -- path = vim.fn.expand("~") .. "/linux/obsidian/obsidian-docs/",
+          path = "~/linux/obsidian/obsidian-docs",
+        },
+      },
+      before_save = function(note)
+        local client = require("obsidian").get_client()
+        local rel_path = note.path:sub(#client.dir + 2)
+        local folders = {}
+        for folder in rel_path:gmatch("([^/]+)/") do
+          table.insert(folders, folder)
+        end
+        note.frontmatter.tags = folders
+        note.frontmatter.aliases = note.frontmatter.aliases or {}
+      end,
+    },
+  },
+}
